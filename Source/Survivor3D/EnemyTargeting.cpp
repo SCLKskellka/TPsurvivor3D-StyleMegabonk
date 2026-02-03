9886765c0b4de4336855d448c0c5535cd6f91384
Christopher Barrera <c.barrera@ensi.lan>
2026-02-03 09:09:32 +0100

    feat: added targeting component (WIP)

---
+++ Source/Survivor3D/EnemyTargeting.cpp
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyTargeting.h"
+
+#include "EnemyData.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Runtime/Engine/Internal/Kismet/BlueprintTypeConversions.h"
+
+
+// Sets default values for this component's properties
+UEnemyTargeting::UEnemyTargeting()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+	if (!EnemyData.IsNull())
+	{
+		EnemyData.DataTable->GetAllRows("",EnemiesData);
+	}
+}
+
+
+// Called when the game starts
+void UEnemyTargeting::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UEnemyTargeting::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+FEnemyData* UEnemyTargeting::SearchClosestEnemy()
+{
+	FEnemyData* ClosestEnemy = EnemiesData[0];
+	FVector3d PlayerLocation = UGameplayStatics::GetPlayerPawn(this, 0)->GetActorLocation();
+	for (int i = 0; i < EnemiesData.Max(); ++i)
+	{
+		if (UKismetMathLibrary::Vector_Distance(PlayerLocation, ClosestEnemy->Position) >
+			UKismetMathLibrary::Vector_Distance(PlayerLocation, EnemiesData[i]->Position))
+		{
+			ClosestEnemy = EnemiesData[i];
+		}
+	}
+	return ClosestEnemy;
+}
+
---
+++ Source/Survivor3D/EnemyTargeting.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyData.h"
+#include "Components/ActorComponent.h"
+#include "EnemyTargeting.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SURVIVOR3D_API UEnemyTargeting : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(RowType="EnemyData"), Category="EnemyData")
+	FDataTableRowHandle EnemyData;
+	
+	// Sets default values for this component's properties
+	UEnemyTargeting();
+
+protected:
+	TArray<FEnemyData*> EnemiesData;
+	
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+	
+private:
+	FEnemyData* SearchClosestEnemy();
+};
